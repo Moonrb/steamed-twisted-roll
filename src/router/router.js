bce1dc08e54b201851c214e621ed660097da1864
项宗捷 <moonrbt@sina.com>
2019-04-15 15:38:52 +0800

    分支开始

--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import {HashRouter as Router,Redirect,Route,Switch} from 'react-router-dom'
+import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
 import App from '../App'
-import Home from '../view/home/home'
-import Category from '../view/home/home'
-import Detail from '../view/home/home'
-import Login from '../view/home/home'
-const  rollRouter = (
+import Home from '../view/home/home' // 首页
+import Category from '../view/category/category' // 导航页
+import Detail from '../view/detail/detail' // 详情页
+import Login from '../view/login/login' // 登录页
+const rollRouter = (
     <Router>
         <App>
             <Switch>
-            <Route path='/home' component={Home}/>
-            <Route path='/category/:id' component={Category}/>
-            <Route path='/detail' component={Detail}/>
-            <Route path='/login' component={Login}/>
-            <Redirect from='/' to='/home'/>
-        </Switch>
+                <Route path='/home' component={Home}/>
+                <Route path='/category/:id' component={Category} exact/>
+                <Route path='/detail/:id' component={Detail} exact/>
+                <Route path='/login' component={Login}/>
+                <Redirect from='/' to='/home'/>
+            </Switch>
         </App>
     </Router>
 );
